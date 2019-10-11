7113cdff9f3c68e62ef628f93b9e999d752e8879
Jan-Erik Walker <jan.walker@students.zhaw.ch>
2019-10-11 19:51:23 +0200

    test

--- projects-server.js
+++ projects-server.js
@@ -10,28 +10,12 @@ const express = require('express'),
 // app.use(morgan('dev'));
 // app.use(express.static(__dirname + '/static_html'));
 app.use(express());
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
 app.use(bodyParser.json())
-
 app.use(cors());
 
 // var issue = [{name: 'Issue1', date: new Date(), priority: 1}];
 var projects = [{ name: 'Projekt1', issues: [{name: 'Issue1', date: new Date(), priority: 1}]}, { name: 'Projekt2', issues: []}, { name: 'Projekt3', issues: []}];
-
-// app.post('/projects', function(req, res) {
-//   todos.push(req.body.project);
-
-//   res.format({
-//     'application/json': function() {
-//       res.status(200).json({'todo': req.body.project});
-//     },
-//     'text/html': function() {
-//       res.redirect(301, '/');
-//     }});
-// });
 
 app.get("/", function(req, res) {
   res.send("Hello World");
