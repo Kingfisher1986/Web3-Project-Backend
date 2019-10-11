9c0f953d56830bcb5368b87298c5ade092510561
Jan-Erik Walker <jan.walker@students.zhaw.ch>
2019-10-11 18:34:29 +0200

    splited frontend and backend into two different projects

---
+++ projects-server.js
@@ -0,0 +1,40 @@
+"use strict";
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      morgan = require('morgan'),
+      cors = require('cors'),
+      app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static_html'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(cors());
+
+var issue = [{name: 'Issue1', date: new Date(), priority: 1}];
+var projects = [{ name: 'Projekt1', issues: [{name: 'Issue1', date: new Date(), priority: 1}]}, { name: 'Projekt2', issues: []}, { name: 'Projekt3', issues: []}];
+
+app.post('/projects', function(req, res) {
+  todos.push(req.body.project);
+
+  res.format({
+    'application/json': function() {
+      res.status(200).json({'todo': req.body.project});
+    },
+    'text/html': function() {
+      res.redirect(301, '/');
+    }});
+});
+
+app.get('/projects', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.status(200).json(projects);
+});
+
+app.listen(8081, function(){
+  console.log("ready captain.");
+});
