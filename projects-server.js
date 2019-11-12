7a0797e32b1c31f5b700133fa3823c330036be19
Jan-Erik Walker <jan.walker@students.zhaw.ch>
2019-11-13 00:20:48 +0100

    added post and put functions

--- projects-server.js
+++ projects-server.js
@@ -1,5 +1,5 @@
 "use strict";
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8081;
 
 const express = require('express'),
       bodyParser = require('body-parser'),
@@ -28,6 +28,21 @@ app.get('/projects', function(req, res) {
   // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   res.status(200).json(projects);
+});
+
+app.post('/project', function(req, res) {
+  console.log("Project added!");
+  projects.push(req.body);
+  // projects = req.body.projects;
+  console.log(projects);
+  res.send(req.body);
+});
+
+app.put('/project', function(req, res) {
+  console.log("Project updated!");
+  var index = projects.findIndex(project => project.name === req.body.name);
+  projects[index].issues = req.body.issues;
+  res.send(req.body);
 });
 
 app.listen(port, function(){
